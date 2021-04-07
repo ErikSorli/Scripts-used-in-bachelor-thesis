89dcf8b40efa06b90d85e8727fc3372caf782930
ErikSorli <69899386+ErikSorli@users.noreply.github.com>
2021-04-07 13:11:46 +0200

    Create send-ccmeval.ps1

---
+++ send-ccmeval.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$False)]
+        [String]$ComputerName = $env:COMPUTERNAME
+        )
+
+# Checks if the computer is running on the correct 
+If ($ComputerName -eq $env:COMPUTERNAME)
+{
+    If (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {
+        Write-Warning "Run cmdlet as Admin!"
+        Return
+    }
+   #Starting the process that checks the current health check for the given computer
+   Write-Verbose "Starting ccm health check for $ComputerName"
+   Start-Process -Filepath "C:\Windows\CCM\CcmEval.exe"
+   do{
+        
+    }while (Get-Process -ComputerName $ComputerName -Name CcmEval.exe -ErrorAction SilentlyContinue)
+    #Sending the report to Config Manager
+    Copy-Item -Path C:\Windows\CCM\CcmEval.xml -Destination 'C:\Users\Erik_adm\CCM_Eval_log' -ToSession(New-PSSession -ComputerName MANAGER)
+}
+ 
