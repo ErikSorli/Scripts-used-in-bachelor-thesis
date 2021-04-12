b11b9332adaae491fd3d7df0ce3ec0117f36469c
ErikSorli <69899386+ErikSorli@users.noreply.github.com>
2021-04-12 10:38:26 +0200

    Update send-ccmeval.ps1

--- send-ccmeval.ps1
+++ send-ccmeval.ps1
@@ -1,24 +1,98 @@
-[CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$False)]
-        [String]$ComputerName = $env:COMPUTERNAME
-        )
-
-# Checks if the computer is running on the correct 
-If ($ComputerName -eq $env:COMPUTERNAME)
+function Get-CCMEvalResult
 {
-    If (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-    {
-        Write-Warning "Run cmdlet as Admin!"
-        Return
-    }
-   #Starting the process that checks the current health check for the given computer
-   Write-Verbose "Starting ccm health check for $ComputerName"
-   Start-Process -Filepath "C:\Windows\CCM\CcmEval.exe"
-   do{
-        
-    }while (Get-Process -ComputerName $ComputerName -Name CcmEval.exe -ErrorAction SilentlyContinue)
-    #Sending the report to Config Manager
-    Copy-Item -Path C:\Windows\CCM\CcmEval.xml -Destination 'C:\Users\Erik_adm\CCM_Eval_log' -ToSession(New-PSSession -ComputerName MANAGER)
-}
+    <#             .Synopsis             Get the results of the most recent client health evaluation on a local or remote computer             .DESCRIPTION             Parses the ccmevalreport.xml file into a readable format to view the results of the ccmeval task.  Can be run on the local or remote computer.             .EXAMPLE             Get-CCMEvalResult             Returns the ccmeval results from the local machine             .EXAMPLE             Get-CCMEvalResult -ComputerName PC001             Returns the ccmeval results from a remote machine             .EXAMPLE             'PC001','PC002' | Get-CCMEvalResult             Returns the ccmeval results from a remote machine     #>
  
+    #requires -Version 2
+ 
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $false,
+                ValueFromPipeline = $true
+        )]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+ 
+    Begin {
+        $Script = {
+            $TargetFile = "C:\Windows\CCM\CcmEvalReport.xml"
+            try
+            {
+                Start-Process -FilePath powershell.exe -ArgumentList "-Command ""&{Copy-Item C:\Users\Administrator\Documents\CcmEvalReport.xml $TargetFile -Force}""" -Wait -ErrorAction Stop  -Verb Runas -WindowStyle Hidden
+            }
+            catch
+            {
+                $_.Exception.Message
+                continue
+            }
+ 
+            if (!(test-path $TargetFile))
+                {
+                    Write-Error -Message "Could not locate the CcmEvalReport.xml"
+                    continue
+                }
+ 
+            $xml = New-Object -TypeName System.Xml.XmlDocument
+            $xml.Load($TargetFile)
+            Remove-Item $TargetFile -Force
+            return $xml
+        }
+    }
+ 
+    Process {
+        if ($ComputerName -eq $env:COMPUTERNAME)
+        {
+            $xml = Invoke-Command -ScriptBlock $Script
+        }
+        Else
+        {
+            try
+                {
+                    $xml = Invoke-Command -ScriptBlock $Script -ComputerName $ComputerName -ErrorAction Stop
+                }
+            catch
+                {
+                    if ($Error[0] | Select-String -Pattern 'Access is denied')
+                        {
+                            $Credentials = $host.ui.PromptForCredential('Credentials required', "Access was denied to $Computername.  Enter credentials to connect.", '', '')
+                            $xml = Invoke-Command -ScriptBlock $Script -ComputerName $ComputerName -Credential $Credentials
+                        }
+                    Else { $_.Exception.Message }
+                }
+        }
+ 
+        $Checks = $xml.ClientHealthReport.HealthChecks.HealthCheck |
+        Select-Object -Property @{
+            l = 'Test'
+            e = {
+                $_.Description
+            }
+        }, @{
+            l = 'Result'
+            e = {
+                $_.'#text'
+            }
+        } |
+        Sort-Object -Property Test
+        [array]$Summary = $xml.ClientHealthReport.Summary |
+        Select-Object -Property @{
+            l = 'ComputerName'
+            e = {
+                $ComputerName
+            }
+        }, @{
+            l = 'EvaluationTime'
+            e = {
+                [datetime]($_.EvaluationTime)
+            }
+        }, @{
+            l = 'Result'
+            e = {
+                $_.'#text'
+            }
+        }
+ 
+        $Summary
+        $Checks | Format-Table
+    }
+}
