9b64f930199c93bfa68240d9348c00e8f90522e7
ErikSorli <69899386+ErikSorli@users.noreply.github.com>
2021-04-16 12:59:29 +0200

    Update send-ccmevalreport.ps1

--- send-ccmevalreport.ps1
+++ send-ccmevalreport.ps1
@@ -3,69 +3,20 @@
         [Parameter(Mandatory=$False)]
         [String]$ComputerName = $env:COMPUTERNAME
         )
- 
-# Code to set 'SendAlways' in registry
-$SendAlways = {
-    Param($Value)
-    $Path = "HKLM:\Software\Microsoft\CCM\CcmEval"
- 
-    Try
-    {
-        $null = New-ItemProperty -Path $Path -Name 'SendAlways' -Value $Value -Force -ErrorAction Stop
-    }
-    Catch
-    {
-        $_
-    }
-}
- 
-# Run against local computer
+
+# Checks if the computer is running on the correct 
 If ($ComputerName -eq $env:COMPUTERNAME)
 {
     If (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
     {
-        Write-Warning "This cmdlet must be run as administrator against the local machine!"
+        Write-Warning "Run cmdlet as Admin!"
         Return
     }
- 
-    Write-Verbose "Enabling 'SendAlways' in registry"
-    $Result = Invoke-Command -ArgumentList "TRUE" -ScriptBlock $SendAlways
- 
-    If (!$Result.Exception)
-    {
-        Write-Verbose "Triggering CM Health Evaluation task"
-        Invoke-Command -ScriptBlock { schtasks /Run /TN "Microsoft\Configuration Manager\Configuration Manager Health Evaluation" /I }
- 
-        Write-Verbose "Waiting for ccmeval to finish"
-        do {} while (Get-process -Name ccmeval -ErrorAction SilentlyContinue)
- 
-        Write-Verbose "Disabling 'SendAlways' in registry"
-        Invoke-Command -ArgumentList "FALSE" -ScriptBlock $SendAlways
-    }
-    Else
-    {
-        Write-Error $($Result.Exception.Message)
-    }
-}
-# Run against remote computer
-Else
-{
-    Write-Verbose "Enabling 'SendAlways' in registry"
-    $Result = Invoke-Command -ComputerName $ComputerName -ArgumentList "TRUE" -ScriptBlock $SendAlways
- 
-    If (!$Result.Exception)
-    {
-        Write-Verbose "Triggering CM Health Evaluation task"
-        Invoke-Command -ComputerName $ComputerName -ScriptBlock { schtasks /Run /TN "Microsoft\Configuration Manager\Configuration Manager Health Evaluation" /I }
- 
-        Write-Verbose "Waiting for ccmeval to finish"
-        do {} while (Get-process -ComputerName $ComputerName -Name ccmeval -ErrorAction SilentlyContinue)
- 
-        Write-Verbose "Disabling 'SendAlways' in registry"
-        Invoke-Command -ComputerName $ComputerName -ArgumentList "FALSE" -ScriptBlock $SendAlways
-    }
-    Else
-    {
-        Write-Error $($Result.Exception.Message)
-    }
+   #Starting the process that checks the current health check for the given computer
+   Write-Verbose "Starting ccm health check for $ComputerName"
+   Start-Process -Filepath "C:\Windows\CCM\CcmEval.exe"
+   do{
+        
+    }while (Get-Process -ComputerName $ComputerName -Name CcmEval.exe -ErrorAction SilentlyContinue)
+    #Sending the report to Config Manager
 }
